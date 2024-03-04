5dd3f4d0da6a13f78f3975a7b778278af228dbbd
Jhon Rayo <rayojhon99@gmail.com>
2024-03-03 21:27:55 -0500

    improve grid manager

--- src/grid_manager.ts
+++ src/grid_manager.ts
@@ -4,13 +4,18 @@ import "hammerjs";
 import { DIRECTION_LEFT, DIRECTION_RIGHT } from "hammerjs";
 
 const MAX_ITEMS_GRID = 9;
+const WAIT_TO_TRIGGER_SWIPE = 2000;
 const isGridManagerEnabled = window.innerWidth <= 640;
+
+let isAnimationRunning =
+  false; /* indicates whether we know the animation is running - it does not reliable tell if it is running */
 
 type SwipeDirection = "left" | "right";
 
 function main() {
   let currentPosInGrid = 0;
   let lastDirection: SwipeDirection | undefined;
+  let lastRealSwipe = 0;
 
   if (!isGridManagerEnabled) {
     return;
@@ -19,7 +24,24 @@ function main() {
   let manager: HammerManager;
   let swipe: SwipeRecognizer;
 
-  const handleSwipeEvent = (e: HammerInput) => {
+  const unsafeHandleProgrammaticSwipeEvent = (direction: SwipeDirection) =>
+    handleSwipeEvent(
+      {
+        offsetDirection:
+          direction === "left" ? DIRECTION_LEFT : DIRECTION_RIGHT,
+      } as unknown as HammerInput,
+      /* isProgrammatic */ true
+    );
+
+  const handleSwipeEvent = (e: HammerInput, isProgrammatic?: boolean) => {
+    if (isAnimationRunning) return;
+    const cachePrevPos = currentPosInGrid;
+    const setIsAnimationRunning = (val: boolean) => {
+      isAnimationRunning = val;
+      setTimeout(() => {
+        isAnimationRunning = !val;
+      }, 900);
+    };
     switch (e.offsetDirection) {
       case DIRECTION_RIGHT:
         switch (true) {
@@ -28,6 +50,7 @@ function main() {
             break;
         }
         lastDirection = "right";
+        setIsAnimationRunning(true);
         break;
       case DIRECTION_LEFT:
         switch (true) {
@@ -36,26 +59,25 @@ function main() {
             break;
         }
         lastDirection = "left";
+        setIsAnimationRunning(true);
         break;
     }
-    rerender();
+    cachePrevPos !== currentPosInGrid && rerender();
+    cachePrevPos !== currentPosInGrid &&
+      !isProgrammatic &&
+      (() => {
+        lastRealSwipe = Date.now();
+      })();
+  };
+
+  const instantiateSwipeManager = () => {
+    manager = new Hammer.Manager(document.getElementById(`services-section`)!);
+    swipe = new Hammer.Swipe();
+    manager.add(swipe);
+    manager.on("swipe", handleSwipeEvent);
   };
 
   const rerender = () => {
-    const instantiateSwipeManager = (inst: number) => {
-      // Destroy old listener
-      swipe && manager?.remove(swipe);
-      manager?.off("swipe");
-
-      // Add new listener
-      manager = new Hammer.Manager(
-        document.getElementById(`gi-${inst + 1}`)?.parentNode!
-      );
-      swipe = new Hammer.Swipe();
-      manager.add(swipe);
-      manager.on("swipe", handleSwipeEvent);
-    };
-
     const el = <HTMLElement>(
       document.getElementById(`gi-${currentPosInGrid + 1}`)?.parentNode!
     );
@@ -69,17 +91,48 @@ function main() {
         )?.parentNode!
       );
 
-    prev?.classList.add("slideOut-mb");
+    const animationClassForPrev =
+      lastDirection === "right" ? "slideOutRight-mb" : "slideOutLeft-mb";
+    const animationClassForCurr =
+      lastDirection === "right" ? "slideInFromLeft-mb" : "slideInFromRight-mb";
+
+    prev?.classList.add(animationClassForPrev);
+    el?.classList.add(animationClassForCurr);
+    el?.classList.remove("hidden-mb");
     setTimeout(() => {
-        el?.classList.remove("hidden-mb");
-        prev?.classList.remove("slideOut-mb");
-        prev?.classList.add("hidden-mb");
+      prev?.classList.remove(animationClassForPrev);
+      prev?.classList.add("hidden-mb");
+      el?.classList.remove(animationClassForCurr);
     }, 1000);
 
-    instantiateSwipeManager(currentPosInGrid);
+    // Update pagination controls
+    lastDirection &&
+      document
+        .getElementById(
+          `grid-item-control-${
+            lastDirection === "right" ? currentPosInGrid + 2 : currentPosInGrid
+          }`
+        )
+        ?.classList.remove("active");
+    document
+      .getElementById(`grid-item-control-${currentPosInGrid + 1}`)
+      ?.classList.add("active");
   };
 
+  setInterval(() => {
+    // Only trigger programmatic swipe if the user hasn't engaged with the content in the last 2 seconds
+    lastRealSwipe + WAIT_TO_TRIGGER_SWIPE < Date.now() &&
+      unsafeHandleProgrammaticSwipeEvent(
+        currentPosInGrid === MAX_ITEMS_GRID - 1
+          ? "right"
+          : currentPosInGrid === 0
+          ? "left"
+          : lastDirection ?? "left"
+      );
+  }, WAIT_TO_TRIGGER_SWIPE);
+
   // Cause a rerender on init to set the initial state
+  instantiateSwipeManager();
   rerender();
 }
 
